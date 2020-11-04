885d6cf98214a9dceb8f9ef9fd4a36f0f811b0a3
Lanash.D <lanashdamodharan@gmail.com>
2020-11-04 11:37:10 +0530

    Problem 1

--- Ways to climb N stairs/Stairs.py
+++ Ways to climb N stairs/Stairs.py
@@ -1,7 +1,9 @@
 No_of_stairs=int(input())  # initialize a vairable to get number of stairs from the user
+
+# initailze a, b, c = 0
 a=0   
 b=1   
-c=1  # initailze a, b, c = 0
+c=1  
 for stairs in range(No_of_stairs-1): #  Create a loop till the range No_of_stairs - 1 
     c=a+b  
     a=b
