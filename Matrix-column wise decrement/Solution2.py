8ad6fff65bdc10812512ec298768af3a39febbf6
Lanash.D <lanashdamodharan@gmail.com>
2020-11-05 18:04:06 +0530

    Problem 2

---
+++ Matrix-column wise decrement/Solution2.py
@@ -0,0 +1,14 @@
+'''
+Sample Input:
+3
+
+Sample Output:
+3 6 9
+2 4 6
+1 2 3
+'''
+N = int(input())
+for row in range(N, 0, -1):
+    for elements in range(1,N+1):
+        print(row * elements, end=" ")
+    print()
