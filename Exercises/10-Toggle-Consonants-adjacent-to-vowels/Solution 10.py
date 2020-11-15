a3080f33703c149fc63067821a76588b18354a63
Lanash.D <lanashdamodharan@gmail.com>
2020-11-15 18:09:36 +0530

    Update exercise

---
+++ Exercises/10-Toggle-Consonants-adjacent-to-vowels/Solution 10.py
@@ -0,0 +1,30 @@
+S = input().strip()
+Slist = []
+for ch in S:
+    Slist.append(ch)
+vowels = ['a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U']
+N = len(S)
+for i in range(N):
+    if i == 0:
+        if Slist[i] not in vowels:
+            if Slist[i+1] in vowels:
+                if Slist[i].isupper():
+                    Slist[i] = S[i].lower()
+                else:
+                    Slist[i] = S[i].upper()
+    elif i == N-1:
+        if Slist[i] not in vowels:
+            if Slist[i-1] in vowels:
+                if Slist[i].isupper():
+                    Slsit[i] = S[i].lower()
+                else:
+                    Slist[i] = S[i].upper()
+    else:
+        if Slist[i] not in vowels:
+            if Slist[i+1] in vowels or Slist[i-1] in vowels:
+                if Slist[i].isupper():
+                    Slist[i] = S[i].lower()
+                else:
+                    Slist[i] = S[i].upper()
+for ch in Slist:
+    print(ch, end="")
