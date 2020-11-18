5216ed8df1a567b840bd8d6c0b534e0ba8a51860
Lanash.D <lanashdamodharan@gmail.com>
2020-11-18 15:54:40 +0530

    Update exercises

--- Exercises/12-Integer-pyramid-pattern/Solution 12.py
+++ Exercises/12-Integer-pyramid-pattern/Solution 12.py
@@ -1,9 +1,23 @@
-N = int(input())
-end = 1
-for i in range(1, N+1):
-    for j in range(N-i):
-        print("*", end=" ")
-    for k in range(1, end+1):
-        print(k, end=" ")
-    end += 2
-    print()
+'''
+
+Sample input: 
+ 5
+
+ Sample output: 
+
+ * * * * 1
+ * * * 1 2 3
+ * * 1 2 3 4 5
+ * 1 2 3 4 5 6 7
+ 1 2 3 4 5 6 7 8 9
+
+'''
+N = int(input())             # store the input in the variable N
+end = 1                      # create a variable end and assign its value to 1
+for i in range(1, N+1):      # for i in range from 1 to N+1
+    for j in range(N-i):            # for j in range from 0 to N-i
+        print("*", end=" ")             # print the symbol '*'
+    for k in range(1, end+1):       # for k in range from 1 to end+1
+        print(k, end=" ")               # print k
+    end += 2                        # increment the value of end by 2
+    print()                         # use a empty print statement for creating a new line
