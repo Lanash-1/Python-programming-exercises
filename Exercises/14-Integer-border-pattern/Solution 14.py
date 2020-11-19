d654a3594779436e92e9ac036dd961a895277a4d
Lanash.D <lanashdamodharan@gmail.com>
2020-11-19 17:15:17 +0530

    update exercises

--- Exercises/14-Integer-border-pattern/Solution 14.py
+++ Exercises/14-Integer-border-pattern/Solution 14.py
@@ -14,13 +14,13 @@ Sample output:
 
 '''
 
-N = int(input())
-count = 1
-for row in range(1, N+1):
-    for col in range(1, N+1):
-        if col == 1 or col == N or row == 1 or row == N:
-            print(count, end=" ")
-            count += 1
-        else:
-            print("*", end=" ")
-    print()
+N = int(input())                    # initialize a variable N and get the input from the user
+count = 1                           # initialize a variable named count and assign its value to 1
+for row in range(1, N+1):           # for every row from 1st to N+1th
+    for col in range(1, N+1):           # for every column from 1st to N+1th
+        if col == 1 or col == N or row == 1 or row == N: # if column is equal to 1 or column is equal to N or row is equal to 1 or row is equal to N
+            print(count, end=" ")                           # print the value of count
+            count += 1                                      # increase the value of count by 1
+        else:                                            # else
+            print("*", end=" ")                             # print the symbol '*'
+    print()                             # use a empty print statement outside the second for loop for a new line
