bbc838b5f56fb24fa8fa1de31393c7ebf075fba2
Lanash.D <lanashdamodharan@gmail.com>
2020-11-23 14:07:51 +0530

    Update exercises

--- Exercises/17-Three-strings/Solution 17.py
+++ Exercises/17-Three-strings/Solution 17.py
@@ -1,12 +1,29 @@
-N = int(input())
+'''
+
+Sample input: 
+
+3
+JOHN
+JOHNY
+JANARDHAN
+
+Sample output: 
+
+JJOJAN
+OHHARD
+NNYHAN
+
+'''
+
+NoOfNames = int(input())   
 names = []
-for i in range(N):
+for i in range(NoOfNames):
     names.append(input().strip())
-for i in range(N):
+for i in range(NoOfNames):
     print((names[i])[:i+1], end="")
 print()
-for i in range(N):
+for i in range(NoOfNames):
     print((names[i])[i+1:len(names[i])-i-1], end="")
 print()
-for i in range(N):
+for i in range(NoOfNames):
     print((names[i])[len(names[i])-i-1:], end="")
