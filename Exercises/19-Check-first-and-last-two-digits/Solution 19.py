c5caf094cd2c893253c4bb976256551ce56d0b39
Lanash.D <lanashdamodharan@gmail.com>
2020-11-26 17:07:02 +0530

    Update exercises

--- Exercises/19-Check-first-and-last-two-digits/Solution 19.py
+++ Exercises/19-Check-first-and-last-two-digits/Solution 19.py
@@ -1,5 +1,26 @@
-N = input().strip()
-if(N[:2] == N[-2:]):
+'''
+
+Sample input 1: 
+
+155215
+
+Sample output 1: 
+
+yes
+.............
+
+Sample input 2: 
+
+9217623
+
+Sample output 2: 
+
+no
+
+'''
+
+Number = input().strip()
+if(Number[:2] == Number[-2:]):
     print("yes")
 else:
     print("no")
